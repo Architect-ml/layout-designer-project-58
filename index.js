c6fbfa8f1e7b7240641e1640fcb90d78a6349db7
michael-lis <mifka.4oki@gmail.com>
2022-11-04 13:40:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-function scrollTo(element) {
-  window.scroll({
-    left: 0,
-    top: element.offsetTop,
-    behavior: 'smooth',
+document.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const href = this.getAttribute('href').substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = 0;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
   });
-}
+});
